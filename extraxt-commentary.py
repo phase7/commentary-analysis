f6de28a184eafe242a4a3205dd1af767ed377630
P7 <ratulroy20@gmail.com>
2019-06-09 14:15:31 +0600

    B I G  U P D A T E collected commentaries for a few matches in the "commentaries folder" other files are just outputs of previous extract commentary.py

--- extraxt-commentary.py
+++ extraxt-commentary.py
@@ -7,9 +7,11 @@ for j in range(12):
     apistring = 'http://site.web.api.espn.com/apis/site/v2/sports/cricket/18815/playbyplay?contentorigin=espn&event=1153635&page=' + str(j) + '&period=1&section=cricinfo'
     r = requests.get(apistring)
     data = json.loads(r.text)
-    
-    for i in range(25):
-        if ( ('text' not in data['commentary']['items'][i] ) or ('shortText' not in data['commentary']['items'][i]) ):
-            continue #For "NO BALL"s, there may not be any commentary, which throws "keyError"
-        print ( data['commentary']['items'][i]['shortText'], " : ", data['commentary']['items'][i]['text'] )
-    print ('\n\n')
+    print (json.dumps(data, indent = 2))
+    input()
+
+    # for i in range(25):
+    #     if ( ('text' not in data['commentary']['items'][i] ) or ('shortText' not in data['commentary']['items'][i]) ):
+    #         continue #For "NO BALL"s, there may not be any commentary, which throws "keyError"
+    #     print ( data['commentary']['items'][i]['shortText'], " : ", data['commentary']['items'][i]['text'] )
+    # print ('\n\n')
